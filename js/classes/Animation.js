e153f7fc77aa7f2ca1b2bc586263b2bb943b7f4e
Oluwadare Seyi <oluwadareseyii@gmail.com>
2023-04-21 12:30:50 +0100

    first commit

---
+++ js/classes/Animation.js
@@ -0,0 +1,53 @@
+import AutoBind from "../utils/bind";
+
+export default class {
+  constructor({ element, elements }) {
+    AutoBind(this);
+
+    const { animationDelay, animationTarget } = element.dataset;
+
+    this.delay = animationDelay;
+
+    this.element = element;
+    this.elements = elements;
+
+    this.target = animationTarget ? element.closest(animationTarget) : element;
+
+    this.isVisible = false;
+    this.hasAnimatedIn = false;
+
+    if ("IntersectionObserver" in window) {
+      this.createObserver();
+
+      this.animateOut();
+    } else {
+      this.animateIn();
+    }
+  }
+
+  createObserver() {
+    this.observer = new window.IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!this.isVisible && entry.isIntersecting) {
+          if (this.delay) {
+            setTimeout(() => {
+              this.animateIn();
+            }, this.delay);
+          } else {
+            this.animateIn();
+          }
+        } else {
+          this.animateOut();
+        }
+      });
+    }).observe(this.target);
+  }
+
+  animateIn() {
+    this.isVisible = true;
+  }
+
+  animateOut() {
+    // this.isVisible = false;
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Welcome to home</h1>
+  <ButtonComponent :loading="false" button-type="primary" @click="click">
+    Hello
+  </ButtonComponent>
+</template>
+
+<script lang="ts" setup>
+const click = () => {
+  console.log("clicked");
+};
+</script>
+
+<style></style>
