3595d038b2bf89e851feb806985d3adcfda2457b
Oluwadare Seyi <oluwadareseyii@gmail.com>
2023-04-26 11:06:27 +0100

    chore: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <header>Header</header>
+    <main><slot /></main>
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
