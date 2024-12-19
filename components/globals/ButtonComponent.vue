a7307c43e78abedd3de23903c056f56cf6f4f24e
Oluwadare Seyi <oluwadareseyii@gmail.com>
2024-12-19 13:55:49 +0000

    fix: tweak button component to use Nuxt Link component

--- components/globals/ButtonComponent.vue
+++ components/globals/ButtonComponent.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-type ButtonType = "primary" | "secondary" | "icon";
-type ButtonColor = "solid" | "outline" | "white";
-type ButtonSize = "small" | "medium" | "large";
+type ButtonType = "primary" | "icon";
+type ButtonColor = "outline" | "black" | "dark";
+const Link = resolveComponent("NuxtLink");
 
 const props = defineProps({
     buttonType: {
@@ -11,10 +11,6 @@ const props = defineProps({
     color: {
         type: String as PropType<ButtonColor>,
         default: "outline",
-    },
-    size: {
-        type: String as PropType<ButtonSize>,
-        default: "medium",
     },
     loading: {
         type: Boolean,
@@ -46,7 +42,6 @@ const classNames = computed((): (string | boolean)[] => [
     "c-button",
     props.buttonType && `c-button--${props.buttonType}`,
     props.color && `c-button--${props.color}`,
-    props.size && `c-button--${props.size}`,
     props.buttonType !== "icon" && props.icon && "c-button--withicon",
     props.buttonType !== "icon" && props.icon && props.reversed && "c-button--reversed",
     props.disabled && "c-button--disabled",
@@ -58,23 +53,10 @@ const click = () => {
 </script>
 
 <template>
-    <component
-        :is="href ? 'nuxt-link' : 'button'"
-        :href="href"
-        :to="href && href.startsWith('/') ? href : ''"
-        :class="classNames"
-        :target="href && !href.startsWith('/') && '_blank'"
-        :disabled="loading || disabled"
-        @click="click"
-    >
-        <span v-if="icon && reversed" class="c-button__icon">
-            <component :is="icon" />
-        </span>
+    <component :is="href ? Link : 'button'" :to="href" :class="classNames" :target="href && !href.startsWith('/') ? '_blank' : ''" :disabled="loading || disabled" @click="click">
+        <span v-if="icon" class="c-button__icon"> </span>
         <span v-if="buttonType !== 'icon'" class="c-button__text" data-button-text>
             <slot />
-        </span>
-        <span v-if="icon && !reversed" class="c-button__icon">
-            <component :is="icon" />
         </span>
     </component>
 </template>
